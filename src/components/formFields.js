d165366b464b19a654401cb38c643ec4aaa9a57a
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-26 11:47:46 -0600

    added some login elements

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
     export class FormInput extends Component {
         render() {
-            const { className, title } = this.props;
+            const { className, title, input, type, placeholder } = this.props;
 
             return (
                 <div className="form-input">
                     <label>{title}</label>
-                    <input className={`${className} form-input`}/>
+                    <input
+                        className={`${className} form-input`}
+                        type={type}
+                        {...input}
+                        placeholder={placeholder}
+                    />
                 </div>
             )
         }
